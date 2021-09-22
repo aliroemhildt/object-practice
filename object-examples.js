504eedda1149a10f5dc5454ef99fb9764f1f04ae
Ali Roemhildt <aliroemhildt@gmail.com>
2021-09-21 23:45:13 -0600

    Create objects

--- object-examples.js
+++ object-examples.js
@@ -2,4 +2,17 @@ var car = {
   make: 'Toyota',
   model: 'Highlander',
   color: 'White'
-}
+};
+
+var animal = {
+  species: 'dog',
+  weightLbs: 40,
+  isTrained: true
+};
+
+var customer = {
+  name: 'Ali',
+  order: = ['cheeseburger','french fries'],
+  orderNum: 33,
+  orderTotal: 15,
+};
